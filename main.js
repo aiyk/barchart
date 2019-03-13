b38fd9d41bfb0b697ff5e14936e5cd2657996ff9
aiyk <aiyk.ekwe@gmail.com>
2019-03-13 16:13:13 +0100

    graph up and running

--- main.js
+++ main.js
@@ -1,17 +1,68 @@
-let userData = {
-    username: "aiyk",
-    password: "pass123"
+let slaCompliance = {
+    JAN: {
+        withinSLA: 12,
+        outsideSLA: 28
+    },
+    FEB: {
+        withinSLA: 80,
+        outsideSLA: 20
+    },
+    MAR: {
+        withinSLA: 92,
+        outsideSLA: 8
+    },
+    APR: {
+        withinSLA: 80,
+        outsideSLA: 20
+    },
+    MAY: {
+        withinSLA: 35,
+        outsideSLA: 65
+    },
+    JUN: {
+        withinSLA: 75,
+        outsideSLA: 25
+    },
+    JUL: {
+        withinSLA: 60,
+        outsideSLA: 40
+    },
+    AUG: {
+        withinSLA: 4,
+        outsideSLA: 96
+    },
+    SEP: {
+        withinSLA: 25,
+        outsideSLA: 75
+    },
+    OCT: {
+        withinSLA: 40,
+        outsideSLA: 60
+    },
+    NOV: {
+        withinSLA: 65,
+        outsideSLA: 35
+    },
+    DEC: {
+        withinSLA: 21,
+        outsideSLA: 49
+    },
 }
 
-const submit = () => {
-    let username = document.getElementById("username").value;
-    let password = document.getElementById("password").value;
-
-    if (username != userData.username || password != userData.password) {
-        document.getElementById("err").style.display = "flex";
-        document.getElementById("success").style.display = "none";
-    } else {
-        document.getElementById("err").style.display = "none";
-        document.getElementById("success").style.display = "flex";
+const dataset = () => {
+    for (var entry in slaCompliance) {
+        if (slaCompliance.hasOwnProperty(entry)) {
+            document.write(`
+                <div class="chart__entry">
+                    <div class="chart__entry__litr border-bottom">` + slaCompliance[entry].outsideSLA + `%</div>
+                    <div class="chart__entry__val">
+                        <div style="height:`+ slaCompliance[entry].outsideSLA + `%;" class="chart__entry__val--red"></div>
+                        <div style="height:` + slaCompliance[entry].withinSLA + `%" class="chart__entry__val--green"></div>
+                    </div>
+                    <div class="chart__entry__litr">` + slaCompliance[entry].withinSLA + `%</div>
+                    <div class="chart__entry__litr">` + entry + `</div>
+                </div>
+            `);
+        }
     }
 }
